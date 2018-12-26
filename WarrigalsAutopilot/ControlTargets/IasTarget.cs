df809db1ac822e950ee57faf4de8ccfe602f3c92
Tanner Swett <tannerswett@gmail.com>
2018-12-26 18:32:36 -0500

    Add a pitch-for-airspeed controller
    This is supposed to be a controller for pitching for indicated airspeed.
    It happens that the "Vessel.indicatedAirSpeed" field doesn't seem to
    give the correct value... I'll have to fix that later.

---
+++ WarrigalsAutopilot/ControlTargets/IasTarget.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2018 by Tanner "Warrigal" Swett.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using UnityEngine;
+
+namespace WarrigalsAutopilot.ControlTargets
+{
+    public class IasTarget : Target
+    {
+        Vessel _vessel;
+
+        public IasTarget(Vessel vessel)
+        {
+            _vessel = vessel;
+        }
+
+        public override string Name => "Indicated airspeed";
+
+        public override float MinSetPoint => 0.0f;
+        public override float MaxSetPoint => 1000.0f;
+        public override int MinSetPointInt => 0;
+        public override int MaxSetPointInt => 1000;
+        public override bool WrapAround => false;
+
+        public override float ProcessVariable { get => (float)_vessel.indicatedAirSpeed; }
+    }
+}
